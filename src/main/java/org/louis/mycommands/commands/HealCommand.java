f7c3ae404887afdf26419f80b68614c8a6fc9e89
LouisSpielt <55889644+LouisSpielt@users.noreply.github.com>
2021-12-17 22:12:23 +0100

    Version: 0.0.5 | Cards Aginst Humanity

--- src/main/java/org/louis/mycommands/commands/HealCommand.java
+++ src/main/java/org/louis/mycommands/commands/HealCommand.java
@@ -7,18 +7,7 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 public class HealCommand implements CommandExecutor {
-    /**
-     * Executes the given command, returning its success.
-     * <br>
-     * If false is returned, then the "usage" plugin.yml entry for this command
-     * (if defined) will be sent to the player.
-     *
-     * @param sender  Source of the command
-     * @param command Command which was executed
-     * @param label   Alias of the command which was used
-     * @param args    Passed command arguments
-     * @return true if a valid command, otherwise false
-     */
+
     @Override
     public boolean onCommand( CommandSender sender, Command command, String label, String[] args) {
 
@@ -35,6 +24,22 @@ public class HealCommand implements CommandExecutor {
                 sender.sendMessage("Nicht Online"); // TODO Chat Message
                 return false;
             }
+            if(!(args[2] == null)) {
+                int Hpint = Integer.parseInt(args[2]);
+            }
+            /**
+             * Executes the given command, returning its success.
+             * <br>
+             * If false is returned, then the "usage" plugin.yml entry for this command
+             * (if defined) will be sent to the player.
+             *
+             * @param sender  Source of the command
+             * @param command Command which was executed
+             * @param label   Alias of the command which was used
+             * @param args    Passed command arguments
+             * @return true if a valid command, otherwise false
+             */
+            int i = 20;
             switch(args.length) {
                 case 1:
                     target.setHealth(20);
@@ -43,22 +48,41 @@ public class HealCommand implements CommandExecutor {
                     target.sendMessage("Du wurdest von"); //TODO Chat Message
                     return false;
                 case 2:
-
+                    i = Integer.parseInt(args[2]);
+                    target.setHealth(i);
+                    target.setFoodLevel(i);
+                    sender.sendMessage("Wurde Geheilt"); //TODO Chat Message
+                    target.sendMessage("Du wurdest von"); //TODO Chat Message
                     return false;
+                case 3:
+                    i = Integer.parseInt(args[2]);
+                    switch (args[3]) {
+                        case "hunger":
+                            target.setFoodLevel(i);
+                            sender.sendMessage("Wurde Geheilt"); //TODO Chat Message
+                            target.sendMessage("Du wurdest von"); //TODO Chat Message
 
+                            return true;
+                        case "health":
+                            target.setHealth(i);
+                            sender.sendMessage("Wurde Geheilt"); //TODO Chat Message
+                            target.sendMessage("Du wurdest von"); //TODO Chat Message
 
-
+                            return true;
+                        case "both":
+                            default:
+                                i = Integer.parseInt(args[2]);
+                                target.setHealth(i);
+                                target.setFoodLevel(i);
+                                sender.sendMessage("Eigentlich Falsch"); //TODO Chat Message
+                                sender.sendMessage("Wurde Geheilt"); //TODO Chat Message
+                                target.sendMessage("Du wurdest von"); //TODO Chat Message
+                            return true;
+                    }
             }
 
             }
-
-
-
-
-
             return false;
         }
-
-
 }
 
